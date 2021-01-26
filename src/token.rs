4971b9a41ca34874ba50194b6e5041ce0ad7c668
Toasa <atsushi.tohyama.160.333@gmail.com>
2021-01-26 21:11:30 +0900

    Implement `Display` method for Token.

--- src/token.rs
+++ src/token.rs
@@ -19,5 +19,27 @@ mod token {
         Function,
         Let,
     }
+
+    use std::fmt;
+    impl fmt::Display for Type {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match *self {
+                Type::Illegal => write!(f, "ILLEGAL"),
+                Type::Eof => write!(f, "EOF"),
+                Type::Ident => write!(f, "IDENT"),
+                Type::Int => write!(f, "Int"),
+                Type::Assign => write!(f, "="),
+                Type::Plus => write!(f, "+"),
+                Type::Comma => write!(f, ","),
+                Type::Semicolon => write!(f, ";"),
+                Type::Lparen => write!(f, "("),
+                Type::Rparen => write!(f, ")"),
+                Type::Lbrace => write!(f, "{{"),
+                Type::Rbrace => write!(f, "}}"),
+                Type::Function => write!(f, "FUNCTION"),
+                Type::Let => write!(f, "LET"),
+            }
+        }
+    }
 }
 
